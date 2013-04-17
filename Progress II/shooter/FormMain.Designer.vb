6edb07673b587cd2d26f0c3e585c7bba83a00ae4
Rahmi Wahyulianti <siiketjiil_amy@yahoo.com>
2013-04-17 15:58:15 +0700

    Progress Ke 2
    Program sudah bisa berjalan, namun belum sempurna..!!

---
+++ Progress II/shooter/FormMain.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormMain))
+        Me.Shooter = New System.Windows.Forms.PictureBox()
+        Me.TimerMain = New System.Windows.Forms.Timer(Me.components)
+        Me.Shot = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.Shooter, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Shot, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Shooter
+        '
+        Me.Shooter.Image = CType(resources.GetObject("Shooter.Image"), System.Drawing.Image)
+        Me.Shooter.Location = New System.Drawing.Point(-12, 556)
+        Me.Shooter.Name = "Shooter"
+        Me.Shooter.Size = New System.Drawing.Size(116, 99)
+        Me.Shooter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.Shooter.TabIndex = 0
+        Me.Shooter.TabStop = False
+        '
+        'TimerMain
+        '
+        Me.TimerMain.Enabled = True
+        Me.TimerMain.Interval = 1
+        '
+        'Shot
+        '
+        Me.Shot.BackColor = System.Drawing.Color.Red
+        Me.Shot.Location = New System.Drawing.Point(41, 498)
+        Me.Shot.Name = "Shot"
+        Me.Shot.Size = New System.Drawing.Size(10, 30)
+        Me.Shot.TabIndex = 1
+        Me.Shot.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(2, 1)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(67, 50)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'FormMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(624, 641)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Shot)
+        Me.Controls.Add(Me.Shooter)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "FormMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "SHOOTER"
+        CType(Me.Shooter, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Shot, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Shooter As System.Windows.Forms.PictureBox
+    Friend WithEvents TimerMain As System.Windows.Forms.Timer
+    Friend WithEvents Shot As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ Progress II/shooter/FormMain.vb
@@ -0,0 +1,106 @@
+﻿Public Class FormMain
+    Dim kanan As Boolean
+    Dim kiri As Boolean
+    Dim kec_segitiga As Integer
+    Dim kec_senjata As Integer
+    Dim kec_senyum As Integer
+    Dim drop_senyum As Integer
+    Dim senyum_kanan As Boolean
+
+    Private Sub TimerMain_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TimerMain.Tick
+        MoveShooter()
+        FireShot()
+        MoveSenyum()
+        CheckGameOver()
+        'CheckHit()
+
+
+    End Sub
+
+    Private Sub FormMain_Keydown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyValue = Keys.Right Then
+            kanan = True
+            kiri = False
+        End If
+        If e.KeyValue = Keys.Left Then
+            kiri = True
+            kanan = False
+        End If
+
+        If e.KeyValue = Keys.Space And Shot.Visible = False Then
+            Shot.Top = Shooter.Top
+            Shot.Left = CInt(Shooter.Left + (Shooter.Width / 2) - (Shot.Width / 2))
+            Shot.Visible = True
+
+        End If
+    End Sub
+    Private Sub MoveShooter()
+        If kanan = True And Shooter.Left + Shooter.Width < Me.ClientRectangle.Width Then
+
+            Shooter.Left += kec_segitiga
+
+        End If
+        If kiri = True And Shooter.Left > Me.ClientRectangle.Left Then
+            Shooter.Left -= kec_segitiga
+
+        End If
+    End Sub
+
+    Private Sub FormMain_KeyUp(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyUp
+        If e.KeyValue = Keys.Right Then
+            kanan = False
+
+        End If
+        If e.KeyValue = Keys.Left Then
+            kiri = False
+        End If
+
+    End Sub
+
+    Private Sub FormMain_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        LoadSettings()
+    End Sub
+
+    Private Sub LoadSettings()
+        kec_senjata = 6
+        kec_segitiga = 3
+        Shot.Visible = False
+        senyum_kanan = True
+        kec_senyum = 5
+        drop_senyum = 50
+
+
+    End Sub
+
+    Private Sub FireShot()
+        If Shot.Visible = True Then
+            Shot.Top -= kec_senjata
+        End If
+        If Shot.Top + Shot.Height < Me.ClientRectangle.Top Then
+            Shot.Visible = False
+
+        End If
+    End Sub
+    Private Sub MoveSenyum()
+        If senyum_kanan = True Then
+            PictureBox1.Left += kec_senyum
+        Else
+            PictureBox1.Left -= kec_senyum
+        End If
+        If PictureBox1.Left + PictureBox1.Width > Me.ClientRectangle.Width Then
+            senyum_kanan = False
+            PictureBox1.Top += drop_senyum
+        End If
+        If PictureBox1.Left < Me.ClientRectangle.Left Then
+            senyum_kanan = True
+            PictureBox1.Top += drop_senyum
+        End If
+    End Sub
+    Private Sub CheckGameOver()
+        If PictureBox1.Top + PictureBox1.Height >= Shooter.Top And PictureBox1.Visible Then
+            TimerMain.Enabled = False
+            MsgBox("Game Over")
+        End If
+    End Sub
+
+End Class
